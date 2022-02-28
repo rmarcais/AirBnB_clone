8af25de816410b7c0e15e6390bd3a3e4db99340d
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-02-28 15:09:20 +0100

    updated base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -12,11 +12,20 @@ import time
 class BaseModel:
     """This class defines all common attributes/methods for other classes."""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """This method is called when a new instance is created."""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(args) == 0 and len(kwargs):
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        setattr(self, key, datetime.strptime(value,
+                                            "%Y-%m-%dT%H:%M:%S.%f"))
+                    else:
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Tells the main program how to print the object."""
