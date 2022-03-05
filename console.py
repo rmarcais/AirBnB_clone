ed37320b5aeed1734ea48b0a8f4787a1d208adf2
Ivan Mickisz <ivan.mickisz3@gmail.com>
2022-03-05 09:59:03 +0100

    trying to correct the console outpout

--- console.py
+++ console.py
@@ -31,12 +31,12 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, line):
         """Quit command to exit the program
         """
-        return True
+        quit()
 
     def do_EOF(self, line):
         """EOF command to exit the program
         """
-        return True
+        quit()
 
     def do_create(self, line):
         """Creates a new instance of BaseModel, saves it and prints the id
